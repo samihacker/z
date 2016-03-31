db3deef928bd8793376a61934a23405387643636
samihacker <samsamsamramezani@gmail.com>
2016-03-31 08:27:24 -0700

    Create xy.lua

---
+++ plugins/xy.lua
@@ -0,0 +1,16 @@
+do
+local function run(msg, matches)
+local bot_id = 174557407-- آیدی اینوایت کننده رو اینجا بنویسید --
+local fbot1 = 99962736-- آیدی اینوایت شونده را اینجا بنویسید --
+    if msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(fbot1) then
+       chat_add_user("chat#id"..msg.to.id, 'user#id'..fbot1, ok_cb, false)
+    end
+end
+
+return {
+  patterns = {
+    "^!!tgservice (.+)$",
+  },
+  run = run
+}
+end
